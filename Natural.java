4f29b0b750c91d73b6c239fa77089465a2df85fc
MythiliL <mythili007.07@gmail.com>
2018-02-04 18:24:41 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Natural
+{
+public static void main(String[] args) 
+   {
+        int num, count, total = 0;
+        System.out.println("Enter the value of n:");
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
+        scan.close();
+        for(count = 1; count <= num; count++)
+            total = total + count;
+        System.out.println("Sum of first "+num+" natural numbers is: "+total);
+    }
+}
